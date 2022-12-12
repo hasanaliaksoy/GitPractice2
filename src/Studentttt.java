7da44bf462b5f9e4a949a84cd4e1f12ed3071fbc
hasanaliaksoy <hasanaliaksoy92@icloud.com>
2022-12-12 14:27:54 +0300

    student task done

---
+++ src/Studentttt.java
@@ -0,0 +1,7 @@
+public class Studentttt {
+    public static void main(String[] args) {
+
+        Test1 irfan = new Test1("irfan", "CydeoSchool", 92, 'a', 'G');
+        System.out.println(irfan);
+    }
+}
--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,95 @@
+import java.util.Arrays;
+
 public class Test1 {
-    public static void main(String[] args) {
+    private String name, schoolName;
+    private int age;
+    private char gender, grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public void setAge(int age) {
+        if (age >= 5 && age <= 90) {
+            this.age = age;
+        }
+    }
+
+    public void setGender(char gender) {
+        if ((gender == 'M' || gender == 'm' || gender == 'F' || gender == 'f')) {
+            this.gender = gender;
+        }
+    }
+
+    public void setGrade(char grade) {
+        if (Arrays.asList('A', 'B', 'C', 'D', 'F').contains(grade))
+            this.grade = grade;
+    }
+
+    public Test1(String name, String schoolName, int age, char gender, char grade) {
+       setAge(age);
+       setGender(gender);
+       setName(name);
+       setGrade(grade);
+       setSchoolName(schoolName);
 
     }
+    public void study(){
+        System.out.println(name+" is studying "+ schoolName);
+    }
+
+    @Override
+    public String toString() {
+        return "Test1{" +
+                "name='" + name + '\'' +
+                ", schoolName='" + schoolName + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", grade=" + grade +
+                '}';
+    }
+
 }
+/*
+1. Create a class named Student:
+            private variables:
+                name, age, gender, grade, schoolName
+
+            Encapsulate all the fileds (at least encapsulate two fields manually)
+                    requirments:
+                        1. age should not be set less than 5 or greater than 90
+                        2. gender should not be set to any chanarcter other than: 'M' and 'F'
+                        3. grade should not be set to any chanacters other than: 'A', 'B', 'C', 'D', and 'F'
+
+            Add a constructor that can set all teh fields when the object is created
+                        (requirments of fileds in the above must be applied)
+
+
+            Methods:
+                study()
+                toString()
+ */
